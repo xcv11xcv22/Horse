67c7371ca7d5b3a5a546af7f626b4c766101b050
gqsdvb135790@gmail.com <gqsdvb135790@gmail.com>
2023-06-19 10:44:59 +0800

    add func plDataToByte plByteToData

--- protocol.py
+++ protocol.py
@@ -15,9 +15,37 @@ def plBytesToStr(bs):
     data = bytesToStr(btag+data)
     return data
 
+def plDataToByte(data):
+    if isinstance(data, int):
+        result = numToBytes(data)
+        return result
 
-a = plStrToBytes(b"aaaaaaaaa")
-b = plBytesToStr(a)
+    elif isinstance(data, str) or isinstance(data, bytes):
+        result = plStrToBytes(data)
+        return result
 
-c = plStrToBytes("中文測試中文測試中文測試")
-d = plBytesToStr(c)
+    raise TypeError('invalid tag ' + type(data))
+
+def plByteToData(data):
+    if not isinstance(data, bytes):
+        raise TypeError('invalid tag ' + type(data))
+
+    btag = data[:1]
+    if btag == b's' or btag == b'b':
+        result = plBytesToStr(data)
+        return result
+
+    result, data = bytesToNum(data)
+    return result
+
+
+
+a = plDataToByte(b"aaaaaaaaa")
+b = (a)
+
+c = plDataToByte("中文測試中文測試中文測試")
+d = plByteToData(c)
+
+e = plDataToByte(999999994654654165)
+f = plByteToData(e)
+k = 1
